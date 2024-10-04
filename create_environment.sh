473bd79e107b119e39bf3a000fbbd5b567a22acb
H-Levison <h.levison@alustudent.com>
2024-10-04 04:43:14 -0700

    Worked on create environment and copied the submissions to this directory

---
+++ create_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#creating directories and subdirectories
+echo "Your working directories are being created..."
+mkdir -p ~/submission_reminder_app/{app,modules,assets,config}
+
+#changing to parent directory
+echo "Changing your working directory to submission_reminder_app..."
+cd ~/submission_reminder_app
+
+
